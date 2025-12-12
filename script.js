f2055d0a704348d5b6a7bc3df4b7872750e79bdb
TaoPower <137190187+TaoPower@users.noreply.github.com>
2025-12-12 16:39:32 +0700

    script.js

--- script.js
+++ script.js
@@ -293,18 +293,7 @@ document.addEventListener('DOMContentLoaded', () => {
        ========================================= */
     
     // 1. Project Data
-    const projects = [
-        {
-            id: 1,
-            title: "T - TEAM Cltoh",
-            subtitle: "Nguyễn Trí - All",
-            category: "Clothes Model",
-            group: "model",
-            badges: ["CD", "CAN", "Độc quyền"],
-            image: "assets/product1.png",
-            imagePlaceholder: "linear-gradient(135deg, #1c1c1c, #333)",
-            icon: "fa-shirt"
-        },
+    const projects = [ /* Thêm Project ở đây */
     ];
 
     // State
@@ -856,3 +845,4 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
